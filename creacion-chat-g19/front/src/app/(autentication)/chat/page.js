dd1e4dc8ab646c59ca8a795659165f3f9ae7a151
camilagiardini <cgiardini@pioix.edu.ar>
2025-09-09 19:46:30 -0300

    front chats

--- creacion-chat-g19/front/src/app/(autentication)/chat/page.js
+++ creacion-chat-g19/front/src/app/(autentication)/chat/page.js
@@ -3,7 +3,23 @@ import Contact from "../../../components/Contact";
 import Button from "../../../components/Button";
 import Message from "../../../components/Message";
 import Input from "../../../components/Input";
+import Title from "@/components/Title";
+import styles from "./chat.styles.css";
+import clsx from "clsx";
 
 export default function Chats(props) {
-    
+    return(
+        <>  
+            <div className="chats">
+                <div className="contenedor-contactos">
+                    <Title title="Chats"></Title>
+                    <Contact></Contact>
+                </div>
+                <div className="contenedor-chat">
+                    <p>acá aparece el chat</p>
+                </div>
+            </div>
+
+        </>
+    )
 }
