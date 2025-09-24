e67b9dd13c951169dd71db68288aafce1d1bba95
scortes123 <scortes@pioix.edu.ar>
2025-09-24 11:49:57 -0300

    cambios en el rgister

--- creacion-chat-g19/front/src/app/(autentication)/register/page.js
+++ creacion-chat-g19/front/src/app/(autentication)/register/page.js
@@ -5,85 +5,66 @@ import Input from "@/components/Input"
 import Link from "next/link"
 import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
-import styles from "./register.styles.css"
+import styles from "@/app/(autentication)/register/register.module.css"
 
 export default function RegisterPage() {
     const [usuarios, setUsuarios] = useState([])
     const [email, setEmail] = useState("")
+    const [nombre, setNombre] = useState("")
     const [password, setPassword] = useState("")
+    const [foto, setFoto] = useState("")
     const [error, setError] = useState("")
     const router = useRouter()
 
-    useEffect(() => {
-        fetch("http://localhost:4006/usuarios")
-            .then(response => response.json())
-            .then(result => setUsuarios(result))
-            .catch(err => console.error("Error al cargar usuarios:", err))
-    }, [])
+    
 
-    async function singUp(e) {
-        e.preventDefault()
-        const nuevoUsuario = {
-            email: email,
-            password: password
-        }
+    // async function singUp(e) {
+    //     e.preventDefault()
+    //     const nuevoUsuario = {
+    //         email: email,
+    //         password: password
+    //     }
 
-        const existe = usuarios.some(u => u.email === nuevoUsuario.email)
+    //     const existe = usuarios.some(u => u.email === nuevoUsuario.email)
 
-        if (existe) {
-            setError("Ese correo ya está registrado")
-            return
-        }
+    //     if (existe) {
+    //         setError("Ese correo ya está registrado")
+    //         return
+    //     }
 
-        try {
-            const response = await fetch("http://localhost:4006/usuarios", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(nuevoUsuario)
-            })
+    //     try {
+    //         const response = await fetch("http://localhost:4006/usuarios", {
+    //             method: "POST",
+    //             headers: { "Content-Type": "application/json" },
+    //             body: JSON.stringify(nuevoUsuario)
+    //         })
 
-            if (response.ok) {
-                router.push("/login")
-            } else {
-                setError("Error al crear el usuario")
-            }
-        } catch (error) {
-            setError("Hubo un problema con el registro")
-        }
+    //         if (response.ok) {
+    //             router.push("/login")
+    //         } else {
+    //             setError("Error al crear el usuario")
+    //         }
+    //     } catch (error) {
+    //         setError("Hubo un problema con el registro")
+    //     }
+    // }
+    function signUp() {
+        console.log("hola")
     }
 
+
+
     return (
-        <div className="login-container">
-            <h2>Crear Cuenta</h2>
-            <form onSubmit={singUp}>
-                <input
-                    type="email"
-                    placeholder="Correo electrónico"
-                    value={email}
-                    onChange={(e) => {
-                        console.log(e.target.value)
-                        setEmail(e.target.value)}
-                    }
-                    required
-                />
-                <input
-                    type="password"
-                    placeholder="Contraseña"
-                    value={password}
-                    onChange={(e) => {
-                        console.log(e.target.value)
-                        setPassword(e.target.value)}
-                    }
-                    
-                    required
-                />
-                <button type="submit">Registrarse</button>
-            </form>
-            {error && <p className="error">{error}</p>}
-            <p>
-                ¿Ya tienes cuenta?{" "}
-                <Link href="/login">Inicia sesión aquí</Link>
-            </p>
+        <div className={styles.Contenedorbody}>
+            <div className={styles.loginContainer}>
+                <h2>Register</h2>
+                <Input page="register" placeholder="Escriba su nombre" type="text" onChange={(e) => {setNombre(e.target.value)}}/>
+                <Input page="register" placeholder="Escriba su Conraseña" type="password" onChange={(e) => {setPassword(e.target.value)}}/>
+                <Input page="register" placeholder="Escriba su mail" type="email" onChange={(e) => {setEmail(e.target.value)}}/>
+                <Input page="register" placeholder="Ponga el enlace de su foto (publico)" type="text" onChange={(e) => {setFoto(e.target.value)}}/>
+                <Button onClick={signUp} page="register" text="hola como estas"></Button>
+            </div>
+            
         </div>
     )
 }
